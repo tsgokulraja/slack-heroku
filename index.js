a4bf42cb8d28b930666fcc42009596655c93bea0
tsgokulraja <59054344+tsgokulraja@users.noreply.github.com>
2020-01-31 11:09:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const http = require("http");
 const bodyParser = require("body-parser");
 const app = express()
 const server =http.createServer(app);
-const router = express.Router();
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -12,9 +11,7 @@ app.use(
 );
 const port = process.env.PORT || 4001;
 server.listen(port, () => console.log(`Listening on port ${port}`));
-router.get("/",(req,res)=>{
-    res.send(<h1>Hello</h1>)
-})
+
 app.post("/",(request,response)=>{
   console.log(request);
     response.send({
